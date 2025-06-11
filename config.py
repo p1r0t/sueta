23ef834cd5b79c335feb09926bc4014aa0dc3ccc
p1r0t <spamp1r0t@gmail.com>
2025-06-11 14:36:30 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = ""
-ADMIN_ID = []
-RECEIVER_ID = 
+BOT_TOKEN = "7526079809:AAH4c6A_S_Ayvf5tficNyFFcf33pfZZcATg"
+ADMIN_ID = [654672729]
+RECEIVER_ID = 654672729
 GPT_PROMPT = "Ты бизнесбот в телеграмм. Ты должен отвечать когда пользователь не в сети. Отвечай не длинными ответами и не форматируй текст.\nЗапрос: "
